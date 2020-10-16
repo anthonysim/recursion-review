4e756b17f0d842f5597be04c5fa3b07ad183786f
Anthony Sim <anthonyysim@gmail.com>
2020-10-15 18:01:18 -0700

    50% done with stringifyJSON

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,16 +3,67 @@
 
 // but you don't so you're going to write it from scratch:
 
+
+// 9,
+// null,
+// true,
+// false,
+// 'Hello world',
+// [],
+// [8],
+
 var stringifyJSON = function(obj) {
   var stringified = '';
 
-  // if obj is equaled to an obj
-  // if obj is an array
-  // if obj is a number
-  // if obj is a string
-  // if obj is a boolean
-  // if obj is null
-  // if obj is undefined
-  // if obj is function
+  if (typeof obj === 'number') {
+    stringified += obj.toString();
+  }
 
+  if (obj === null) {
+    stringified += 'null';
+  }
+
+  if (typeof obj === 'boolean') {
+    stringified += '' + obj + '';
+  }
+
+  if (typeof obj === 'string') {
+    stringified += '"' + obj + '"';
+  }
+
+  if (Array.isArray(obj) ) {
+    stringified += '[';
+
+    for (let i = 0; i < obj.length; i++) {
+      stringified += stringifyJSON(obj[i]); // ---> stringifyJSON() = function -> return value d
+      if (i < obj.length - 1) {
+        stringified += ',';
+      }
+    }
+    stringified += ']';
+  }
+
+  if (typeof obj === 'object' && obj !== null && Array.isArray(obj) === false) {
+
+    stringified += '{';
+
+
+    for (let key in obj) {
+
+      if (Array.isArray(obj[key])) {
+        stringified += `"${key}":${stringifyJSON(obj[key])}`;
+
+      } else if (typeof obj === 'object' && obj !== null && Array.isArray(obj) === false) {
+        stringified += `"${key}":${stringifyJSON(obj[key])}`;
+
+      } else {
+        stringified += `"${key}":"${obj[key]}"`;
+      }
+
+    }
+    stringified += '}';
+
+  }
+
+  return stringified;
 };
